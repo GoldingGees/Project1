fe74c349779941bbb0e0819d6163b51ff3c7a4d1
GoldingGees <goldest491@gmail.com>
2022-12-06 14:59:37 -0800

    third commit with js

---
+++ index.js
@@ -0,0 +1,12 @@
+function validate () {
+    let username = document.getElementById('Username');
+    value;
+    let password = document.getElementById('Password');
+    value;
+}
+
+if (Username === 'Golder' && Password === 'learn') {
+    alert('Login successful!');
+} else {
+    alert('Login fialed!');
+}
